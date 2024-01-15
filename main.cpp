d8b99849b08400971530497332c015288f6f744d
Fang Yongrui <yongruifang@163.com>
2024-01-15 16:10:13 +0800

    feat(cmake): use globing to simplify the dependence
    globing usage, and c/c++ try/catch usage

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 #include "operations.h"
 
 int main(){
-    foo();
-    std::cout << "5 + 10" << " = " << add(5,10) << std::endl;
-    std::cout << "5.2 + 10.2" << " = " << add(5.2,10.2) << std::endl;
-    std::cout << "5 - 10" << " = " << subtract(5,10) << std::endl;
-    std::cout << "5 * 10" << " = " << multiply(5,10) << std::endl;
-    std::cout << "5 / 10" << " = " << divide(5,10) << std::endl;
-    std::cout << "5 / 0" << " = " << divide(5, 0) << std::endl;
+    try {
+        foo();
+        std::cout << "5 + 10" << " = " << add(5,10) << std::endl;
+        std::cout << "5.2 + 10.2" << " = " << add(5.2,10.2) << std::endl;
+        std::cout << "5 - 10" << " = " << subtract(5,10) << std::endl;
+        std::cout << "5 * 10" << " = " << multiply(5,10) << std::endl;
+        std::cout << "5 / 10" << " = " << divide(5,10) << std::endl;
+        std::cout << "5 / 0" << " = " << divide(5, 0) << std::endl;
+    }catch(const std::exception& e) {
+        std::cerr << "Exception caught: "<< e.what() << std::endl;
+    }
     return 0;
 }
